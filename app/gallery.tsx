26ed069d418189d23f7fd11d9572968c4d01bba5
Romel Rey Silva <sromelrey@gmail.com>
2024-02-15 19:33:51 +0800

    fix classes typo in modal

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -24,7 +24,7 @@ const Gallery = ({ users }: GalleryProps) => {
   const handleModalOpen = (id: number) => {
     const user = usersList.find((item) => item.id === id) || null;
 
-    if(user) {
+    if (user) {
       setSelectedUser(user);
       setIsModalOpen(true);
     }
@@ -36,49 +36,49 @@ const Gallery = ({ users }: GalleryProps) => {
   };
 
   return (
-    <div className="user-gallery">
-      <h1 className="heading">Users</h1>
-      <div className="items">
+    <div className='user-gallery'>
+      <h1 className='heading'>Users</h1>
+      <div className='items'>
         {usersList.map((user, index) => (
           <div
-            className="item user-card"
+            className='item user-card'
             key={index}
             onClick={() => handleModalOpen(user.id)}
           >
-            <div className="body">
+            <div className='body'>
               <Avatar
                 size={96}
                 name={user.name}
-                variant="marble"
+                variant='marble'
                 colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
               />
             </div>
-            <div className="info">
-              <div className="name">{user.name}</div>
-              <div className="company">{user.company.name}</div>
+            <div className='info'>
+              <div className='name'>{user.name}</div>
+              <div className='company'>{user.company.name}</div>
             </div>
           </div>
         ))}
         <Modal isOpen={isModalOpen} onClose={handleModalClose}>
-          <div className="user-panel">
-            <div className="header">
+          <div className='user-panel'>
+            <div className='header'>
               <div
-                role="button"
+                role='button'
                 tabIndex={0}
-                className="close"
+                className='close'
                 onClick={handleModalClose}
               >
                 <FaRegCircleXmark size={32} />
               </div>
             </div>
-            <div className="body">
+            <div className='body'>
               {selectedUser && (
-                <div className="user-info info">
-                  <div className="avatar">
+                <div className='user-info info'>
+                  <div className='avatar'>
                     <Avatar
                       size={240}
                       name={selectedUser.name}
-                      variant="marble"
+                      variant='marble'
                       colors={[
                         "#92A1C6",
                         "#146A7C",
@@ -88,24 +88,24 @@ const Gallery = ({ users }: GalleryProps) => {
                       ]}
                     />
                   </div>
-                  <div className="name">
+                  <div className='name'>
                     {selectedUser.name} ({selectedUser.username})
                   </div>
-                  <div className="field">
-                    <FaLocationDot className="icon" />
-                    <div className="data">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
+                  <div className='field'>
+                    <FaLocationDot className='icon' />
+                    <div className='value'>{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
                   </div>
-                  <div className="field">
-                    <FaPhone className="icon" />
-                    <div className="value">{selectedUser.phone}</div>
+                  <div className='field'>
+                    <FaPhone className='icon' />
+                    <div className='value'>{selectedUser.phone}</div>
                   </div>
-                  <div className="fields">
-                    <FaEnvelope className="icon" />
-                    <div className="value">{selectedUser.email}</div>
+                  <div className='field'>
+                    <FaEnvelope className='icon' />
+                    <div className='value'>{selectedUser.email}</div>
                   </div>
-                  <div className="company">
-                    <div className="name">{selectedUser.company.name}</div>
-                    <div className="catchphrase">
+                  <div className='company'>
+                    <div className='name'>{selectedUser.company.name}</div>
+                    <div className='catchphrase'>
                       {selectedUser.company.catchPhrase}
                     </div>
                   </div>
