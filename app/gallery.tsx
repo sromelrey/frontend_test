26f93af63150206b100436ca4735228ab955fe9d
Romel Rey Silva <sromelrey@gmail.com>
2024-02-15 16:56:47 +0800

    feat: Added Sorting users functionality

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Avatar from "boring-avatars";
 import {
   FaRegCircleXmark,
@@ -21,6 +21,8 @@ const Gallery = ({ users }: GalleryProps) => {
   const [usersList, setUsersList] = useState(users);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sortField, setSortField] = useState("name");
+  const [sortDirection, setSortDirection] = useState("ascending");
 
   const handleModalOpen = (id: number) => {
     const user = usersList.find((item) => item.id === id) || null;
@@ -36,53 +38,89 @@ const Gallery = ({ users }: GalleryProps) => {
     setIsModalOpen(false);
   };
 
+  const handleSortChange = (sortField: string, sortDirection: string) => {
+    setSortDirection(sortDirection);
+    setSortField(sortField);
+    const sortedUsers = [...usersList].sort((userA, userB) => {
+      // Extract the sort fields from each user object
+      const valueA = getValue(userA, sortField);
+      const valueB = getValue(userB, sortField);
+
+      if (valueA < valueB) {
+        return sortDirection === "ascending" ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return sortDirection === "ascending" ? 1 : -1;
+      }
+      return 0;
+    });
+
+    setUsersList(sortedUsers);
+  };
+
+  // Helper function to get the value of a nested field in an object
+  const getValue = (object: any, field: string) => {
+    return field
+      .split(".")
+      .reduce(
+        (obj: { [x: string]: any }, key: string | number) => obj[key],
+        object
+      );
+  };
+
   return (
-    <div className="user-gallery">
-      <div className="heading">
-        <h1 className="title">Users</h1>
-        <Controls />
+    <div className='user-gallery'>
+      <div className='heading'>
+        <h1 className='title'>Users</h1>
+        <Controls
+          sortField={sortField}
+          sortDirection={sortDirection}
+          onSortChange={handleSortChange}
+          // setSortField={setSortField}
+          // setSortDirection={setSortDirection}
+        />
       </div>
-      <div className="items">
+      <div className='items'>
         {usersList.map((user, index) => (
           <div
-            className="item user-card"
+            className='item user-card'
             key={index}
             onClick={() => handleModalOpen(user.id)}
           >
-            <div className="body">
+            <div className='body'>
               <Avatar
                 size={96}
                 name={user.name}
-                variant="marble"
+                variant='marble'
                 colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
               />
             </div>
-            <div className="info">
-              <div className="name">{user.name}</div>
-              <div className="company">{user.company.name}</div>
+            <div className='info'>
+              <div className='name'>{user.name}</div>
+              <div className='company'>{user.company.name}</div>
             </div>
           </div>
         ))}
         <Modal isOpen={isModalOpen} onClose={handleModalClose}>
-          <div className="user-panel">
-            <div className="header">
+          <div className='user-panel'>
+            <div className='header'>
               <div
-                role="button"
+                role='button'
                 tabIndex={0}
-                className="close"
+                className='close'
                 onClick={handleModalClose}
               >
                 <FaRegCircleXmark size={32} />
               </div>
             </div>
-            <div className="body">
+            <div className='body'>
               {selectedUser && (
-                <div className="user-info info">
-                  <div className="avatar">
+                <div className='user-info info'>
+                  <div className='avatar'>
                     <Avatar
                       size={240}
                       name={selectedUser.name}
-                      variant="marble"
+                      variant='marble'
                       colors={[
                         "#92A1C6",
                         "#146A7C",
@@ -92,24 +130,24 @@ const Gallery = ({ users }: GalleryProps) => {
                       ]}
                     />
                   </div>
-                  <div className="name">
+                  <div className='name'>
                     {selectedUser.name} ({selectedUser.username})
                   </div>
-                  <div className="field">
-                    <FaLocationDot className="icon" />
-                    <div className="data">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
+                  <div className='field'>
+                    <FaLocationDot className='icon' />
+                    <div className='data'>{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
                   </div>
-                  <div className="field">
-                    <FaPhone className="icon" />
-                    <div className="value">{selectedUser.phone}</div>
+                  <div className='field'>
+                    <FaPhone className='icon' />
+                    <div className='value'>{selectedUser.phone}</div>
                   </div>
-                  <div className="fields">
-                    <FaEnvelope className="icon" />
-                    <div className="value">{selectedUser.email}</div>
+                  <div className='fields'>
+                    <FaEnvelope className='icon' />
+                    <div className='value'>{selectedUser.email}</div>
                   </div>
-                  <div className="company">
-                    <div className="name">{selectedUser.company.name}</div>
-                    <div className="catchphrase">
+                  <div className='company'>
+                    <div className='name'>{selectedUser.company.name}</div>
+                    <div className='catchphrase'>
                       {selectedUser.company.catchPhrase}
                     </div>
                   </div>
